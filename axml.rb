5aa74d6c96534920d9b4774b5e0d894cae9bb5ff
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2010-08-08 17:43:43 +0200

    initial import

---
+++ axml.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'nokogiri'
+
+module AxML
+  
+  def self.transform(axml)
+    doc = Nokogiri::XML::Document.new
+    transform_element(axml, doc)
+    doc
+  end
+
+  def self.transform_element(axml, doc, parent = doc)
+    el = parent.add_child(Nokogiri::XML::Element.new(axml[0].to_s, doc))
+    
+    axml[1..-1].each do |e|
+      case e 
+      when Array
+        el.add_child(transform_element(e, doc, el))
+      when String
+        el.content += e
+      when Fixnum
+        el.content += e.to_s
+      when Hash
+        e.each do |k,v|
+          el[k.to_s] = v
+        end
+      end
+    end
+
+    el
+  end
+
+end
---
+++ spec/basic_spec.rb
@@ -0,0 +1,44 @@
+require Pathname.new(__FILE__).dirname.join('spec_helper')
+
+describe AxML do
+
+  describe 'only root' do 
+    
+    before :each do
+      @doc = AxML.transform([:p, 'hey'])
+    end
+
+    it 'should convert to a nokogiri tree' do
+      @doc.should be_a(Nokogiri::XML::Document)
+    end
+
+    it 'should contain one p element containing hey' do
+      @doc.children.size.should == 1
+      @doc.root.node_name.should == 'p'
+      @doc.root.content.should == 'hey'
+    end
+
+  end
+
+  it 'should allow nesting' do
+    doc = AxML.transform([:foo, 'zing', [:bar, [:baz, "hello"], [:another, "here"]]])
+    doc.root.node_name.should == 'foo'
+    doc.root.content == 'zing'
+    doc.root.children.size.should == 2
+    doc.root.children[0].should be_a(Nokogiri::XML::Text)
+    bar = doc.root.children[1]
+    bar.node_name.should == 'bar'
+    bar.children.size.should == 2
+    bar.children[0].node_name.should == 'baz'
+    bar.children[0].content.should == 'hello'
+    bar.children[1].node_name.should == 'another'
+    bar.children[1].content.should == 'here'
+  end
+
+  it 'should allow attributes' do
+    doc = AxML.transform([:foo, { :bar => 'baz', :qux => '123' }, { :bar => 'heh!'}])
+    doc.root.attributes['bar'].content.should == 'heh!'
+    doc.root.attributes['qux'].content.should == '123'
+  end
+  
+end
